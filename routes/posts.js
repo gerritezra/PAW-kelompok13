5f387c54e50da37ba63e4c9a3e20e8e304d3aec7
gerritezra <67199284+gerritezra@users.noreply.github.com>
2021-09-29 02:36:17 +0700

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,22 @@
 import express from 'express';
-import {getMahasiswa, createMahasiswa} from '../controllers/posts.js'
+import {getMahasiswa, getMahasiswaById, createMahasiswa, updateMahasiswa, deleteMahasiswa} from '../controllers/posts.js'
 
 const router = express.Router();
 
+// Route get All Mahasiswa
 router.get('/',getMahasiswa);
+
+//Route get single Mahasiswa
+router.get('/:id',getMahasiswaById);
+
+// Route CREATE Mahasiswa
 router.post('/',createMahasiswa);
 
+//Route get UPDATE Mahasiswa
+router.patch('/:id',updateMahasiswa);
+
+//Route get DELETE Mahasiswa
+router.delete('/:id',deleteMahasiswa);
+
+//export router
 export default router;
