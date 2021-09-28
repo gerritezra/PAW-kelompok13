25c21bfa1ffc175ac7859659b77a4a333803efba
naufalhalims <naufalhalim@mail.ugm.ac.id>
2021-09-28 21:22:08 +0700

    update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true , useUnifiedTopology : t
 
 app.get("/", (req, res) => {
 res.json({
-message: "REST API MAHASISWA CRUD Berhasil"});
+message: "REST API MAHASISWA CRUD telah Berhasil"});
 });
 
 
