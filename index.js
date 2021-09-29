32d866bb238799bc7cbc194d5f351d5289569124
Alvin Indra Kurniawan <83466710+alviindrakurniawan@users.noreply.github.com>
2021-09-29 09:58:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import router from './routes/posts.js';
 
 const app = express();
 
-app.use('/mahasiswa',mahasiwaRoutes)
-app.use(bodyParser.json({ limit :"50mb",extended : true }));
-app.use(bodyParser.urlencoded({ limit :"50mb",extended : true }));
+app.use(express.json())
+app.use(express.urlencoded());
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://alvin:alvin123@cluster0.7w3ev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
+app.use('/mahasiswa',mahasiwaRoutes)
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true , useUnifiedTopology : true })
 	.then(() => app.listen(PORT,() => console.log(`server running on port : ${PORT}`)))
