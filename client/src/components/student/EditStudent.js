3f29ca4c02aa7b57cfdf592601ca3f5bf5c47c06
gerritezra <67199284+gerritezra@users.noreply.github.com>
2021-11-24 01:36:19 +0700

    Update EditStudent.js

--- client/src/components/student/EditStudent.js
+++ client/src/components/student/EditStudent.js
@@ -4,20 +4,20 @@ import { Link, useHistory } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
 
 const EditStudent = () => {
-  const { editStudent, student, majors } = useAppContext();
+  const { editStudent, student} = useAppContext();
 
   const history = useHistory();
 
   const [formData, setFormData] = useState({
-    firstname: student.firstname,
-    lastname: student.lastname,
-    age: student.age,
-    major: student.major._id,
-    email: student.email,
-    phone: student.phone,
+    Nama : student.Nama,
+    NIM : student.NIM,
+    Prodi : student.Prodi,
+    Fakultas : student.Fakultas,
+    Angkatan : student.Angkatan,
+    Status : student.Status,
   });
 
-  const { firstname, lastname, age, major, email, phone } = formData;
+  const { Nama, NIM, Prodi, Fakultas, Angkatan, Status } = formData;
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -29,12 +29,12 @@ const EditStudent = () => {
     editStudent(student._id, formData);
 
     setFormData({
-      firstname: "",
-      lastname: "",
-      age: 0,
-      major: "",
-      email: "",
-      phone: "",
+      Nama : "",
+      NIM : "",
+      Prodi : "",
+      Fakultas : "",
+      Angkatan : "",
+      Status : "",
     });
 
     history.push("/");
@@ -47,94 +47,75 @@ const EditStudent = () => {
       </h3>
       <form onSubmit={handleEditStudent}>
         <div className='form-group'>
-          <label>First Name</label>
+          <label>Nama</label>
           <input
             type='text'
             required
             className='form-control'
-            name='firstname'
-            value={firstname}
+            name='Nama'
+            value={Nama}
             onChange={handleChange}
           />
         </div>
 
         <div className='form-group'>
-          <label>Last Name</label>
+          <label>NIM</label>
           <input
             type='text'
             required
             className='form-control'
-            name='lastname'
-            value={lastname}
+            name='NIM'
+            value={NIM}
             onChange={handleChange}
           />
         </div>
 
         <div className='form-group'>
-          <label>Age</label>
+          <label>Program Studi</label>
+          <input
+            type='text'
+            required
+            className='form-control'
+            name='Prodi'
+            value={Prodi}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Fakultas</label>
+          <input
+            type='text'
+            required
+            className='form-control'
+            name='Fakultas'
+            value={Fakultas}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Angkatan</label>
           <input
             type='number'
             required
             className='form-control'
-            name='age'
-            value={age}
+            name='Angkatan'
+            value={Angkatan}
             onChange={handleChange}
           />
         </div>
 
         <div className='form-group'>
-          <label>Major</label>
-          <select
-            required
-            className='form-control'
-            name='major'
-            value={major}
-            onChange={handleChange}
-          >
-            <option value='' disabled>
-              Select a major
-            </option>
-            {majors.map((item) => (
-              <option value={item._id} key={item._id}>
-                {item.major}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className='form-group'>
-          <label>Email</label>
-          <input
-            type='email'
-            required
-            className='form-control'
-            name='email'
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div className='form-group'>
-          <label>Phone</label>
+          <label>Status</label>
           <input
             type='text'
             required
             className='form-control'
-            name='phone'
-            value={phone}
+            name='Status'
+            value={Status}
             onChange={handleChange}
           />
-        </div>
-
-        <div className='form-group'>
-          <input
-            type='submit'
-            value='Update Student'
-            className='btn btn-success'
-          />{" "}
-          <Link to='/' className='btn btn-secondary'>
-            Cancel
-          </Link>
         </div>
       </form>
     </div>
