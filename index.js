958393b1ef35267dff90e77df34fa1efc79bd95c
gerritezra <67199284+gerritezra@users.noreply.github.com>
2021-11-23 16:17:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 import mahasiwaRoutes from './routes/posts.js';
+import kontakmahasiwaRoutes from './routes/postKontaks.js';
 import router from './routes/posts.js';
 
 const app = express();
@@ -15,6 +17,7 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://alvin:alvin123@cluster0.7w3ev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 app.use('/mahasiswa',mahasiwaRoutes)
+app.use('/kontakmahasiswa',kontakmahasiwaRoutes)
 
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true , useUnifiedTopology : true })
 	.then(() => app.listen(PORT,() => console.log(`server running on port : ${PORT}`)))
