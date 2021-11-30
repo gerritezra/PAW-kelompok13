b9661f9286e639aaa8ac16f4ca936ca2c15becbd
gerritezra <67199284+gerritezra@users.noreply.github.com>
2021-11-30 11:21:06 +0700

    Delete client/src directory

--- client/src/context/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-export const STUDENTS_GET = "STUDENTS_GET";
-export const STUDENT_GET = "STUDENT_GET";
-export const STUDENT_ADD = "STUDENT_ADD";
-export const STUDENT_EDIT = "STUDENT_EDIT";
-export const STUDENT_DELETE = "STUDENT_DELETE";
-export const STUDENT_ERROR = "STUDENT_ERROR";
